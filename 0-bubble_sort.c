23a1eaa55df70820fbf80f848f65a6ab69ce56e8
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-10-20 20:44:34 -0500

    [FIX] deleted space in line

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,23 +9,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0, j = 0;
-    int tmp = 0;
+	size_t i = 0, j = 0;
+	int tmp = 0;
 
-    if (size < 2)
-        return ;
+	if (size < 2)
+		return;
 
-    for (; i < size; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
