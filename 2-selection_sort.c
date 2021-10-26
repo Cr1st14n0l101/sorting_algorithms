8132d7048aba183135580df49dd0e6fbdf4a13d1
Juan Bocanegra <jbocane6@gmail.com>
2021-10-25 23:57:22 -0500

    file 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,26 +1,6 @@
 #include "sort.h"
 
 /**
- * swap - change the values of two variables
- * @array: is an array os integers
- * @size: is the size of the array
- * @a: the first integer
- * @b: the second integer
- * Return: Void
- */
-void swap(int *array, size_t size, int *a, int *b)
-{
-	int t = *a;
-
-	if (*a != *b)
-	{
-		*a = *b;
-		*b = t;
-		print_array((const int *)array, size);
-	}
-}
-
-/**
  * selection_sort - Sorts an array of integers in ascending order
  * using the Selection sort algorithm.
  * @array: Array of integers.
@@ -29,20 +9,27 @@ void swap(int *array, size_t size, int *a, int *b)
 void selection_sort(int *array, size_t size)
 {
 	size_t i = 0, j;
+	int bubble;
 
 	if (!array || size <= 0)
 		return;
 
 	while (i < size)
 	{
-		j = 0;
+		j = i + 1;
 		while (j < size)
 		{
-			if (array[i] < array[j])
-				swap(array, size, &array[i], &array[j]);
+			if (array[i] > array[j])
+			{
+				bubble = array[i];
+				array[i] = array[j];
+				array[j] = bubble;
+				j++;
+			}
 			else
 				j++;
 		}
+		print_array((const int *)array, size);
 		i++;
 	}
 }
