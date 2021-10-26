c942db10b57c9dbfc28e3b1ce5677de2080844c7
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-10-25 19:01:38 -0500

    [FIX] betty corrections

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,26 +9,26 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t i, j, n, gap;
-    int tmp;
+	size_t i, j, n, gap;
+	int tmp;
 
-    if (!array || !size)
-        return;
+	if (!array || !size)
+		return;
 
-    n = 1;
-    while (n < size)
-        n = n * 3 + 1;
-    gap = (n - 1) / 3;
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	gap = (n - 1) / 3;
 
-    for (; gap; gap = (gap - 1) / 3)
-    {
-        for(i = gap; i < size; i++)
-        {
-            tmp = array[i];
-            for(j = i; j >= gap && array[j - gap] > tmp; j -= gap)
-                array[j] = array[j-gap];
-            array[j] = tmp;
-        }
-        print_array(array, size);
-    }
+	for (; gap; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = tmp;
+		}
+		print_array(array, size);
+	}
 }
